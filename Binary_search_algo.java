6b11ca0e73109e7b49eebc37f48a3b711eeb7c34
Ansh Jaiswal <anshjaiswal293@gmail.com>
2026-07-28 17:53:12 +0530

    change class name

--- Binary_search_algo.java
+++ Binary_search_algo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class arrays {
+public class Binary_search_algo {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new int[6];
